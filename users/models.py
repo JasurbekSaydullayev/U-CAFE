bcb5e323a9f20fd2ebf193b2374a69e0e007e98e
Jasurbek <jasurbek.saydullayev@list.ru>
2024-06-10 11:19:12 +0500

    first commit

---
+++ users/models.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+user_type = (
+    ('Admin', 'Admin'),
+    ('Courier', 'Courier'),
+    ('Barmen', 'Barmen'),
+    ('Waitress', 'Waitress'),
+    ('Customer', 'Customer'),
+)
+
+
+class User(AbstractUser):
+    type = models.CharField(max_length=20, choices=user_type, default='Customer')
+    full_name = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=20, blank=True, null=True, unique=True)
+    password = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return self.username
