4749414c071ae6ed509658a626ec8ce3a07a195f
Jasurbek <jasurbek.saydullayev@list.ru>
2024-07-16 10:31:53 +0500

    updated

--- ucafe/urls.py
+++ ucafe/urls.py
@@ -22,8 +22,8 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
     # v1
     path('api/v1/', include('users.api.v1.urls')),
