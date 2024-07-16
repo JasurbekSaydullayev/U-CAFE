3b6cd68a9cc07ffbdf38d766c552b4db087c8b46
Jasurbek <jasurbek.saydullayev@list.ru>
2024-07-16 15:05:45 +0500

    updated

--- foods/api/v1/views.py
+++ foods/api/v1/views.py
@@ -42,7 +42,7 @@ class PhotoViewSet(viewsets.ModelViewSet):
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
-        if serializer.is_valid():
+        if serializer.is_valid(): #
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
