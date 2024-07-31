1ad82dea312a02b74d05685d2e94abcdc01ddedf
Jasurbek <jasurbek.saydullayev@list.ru>
2024-07-31 09:57:17 +0500

    updated

--- expenses/api/v1/views.py
+++ expenses/api/v1/views.py
@@ -1,10 +1,20 @@
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
+from orders.DRY import dry
 from permissions import IsAdmin
 from .serializers import ExpenseSerializer
 from expenses.models import Expenses
 from pagination import StandardResultsSetPagination
+
+manual_parameters = [
+    openapi.Parameter('start_date', in_=openapi.IN_QUERY, type=openapi.TYPE_STRING, format='date',
+                      description='Start date in YYYY-MM-DD format'),
+    openapi.Parameter('end_date', in_=openapi.IN_QUERY, type=openapi.TYPE_STRING, format='date',
+                      description='End date in YYYY-MM-DD format'),
+]
 
 
 class ExpenseViewSet(viewsets.ModelViewSet):
@@ -13,8 +23,10 @@ class ExpenseViewSet(viewsets.ModelViewSet):
     pagination_class = StandardResultsSetPagination
     permission_classes = [IsAuthenticated, IsAdmin]
 
+    @swagger_auto_schema(manual_parameters=manual_parameters)
     def list(self, request, *args, **kwargs):
-        queryset = self.filter_queryset(self.get_queryset()).order_by('-date')
+        start_date, end_date = dry(request)
+        queryset = Expenses.objects.filter(date__range=(start_date, end_date)).order_by('-date')
         page = self.paginate_queryset(queryset)
         if page is not None:
             serializer = self.get_serializer(page, many=True)
